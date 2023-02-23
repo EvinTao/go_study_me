343fcd7bd276904c1daa5a86abbb570a2bc07cfa
EvinTao <taoyingfeng@gmail.com>
2023-02-23 17:28:41 +0800

    flag 包体主要用于命令行解析

---
+++ base/go_init.go
@@ -0,0 +1,26 @@
+package main
+
+import "flag"
+
+var gopath string
+
+func init() {
+	println("init a")
+}
+
+func init() {
+	println("init b")
+}
+
+func init() {
+	println("init c")
+	// gopath may be overridden by --gopath flag on command line.
+	flag.StringVar(&gopath, "gopath", "/root/go", "override default GOPATH")
+}
+
+func main() {
+	println("main -->")
+	// flag 包体主要用于命令行解析
+	flag.Parse()
+	println(gopath)
+}
