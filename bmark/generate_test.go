27bb461e002e7a94c22b33fd5ef65461e7fbf789
evintao <ryantao@gizaworks.com>
2024-09-10 16:36:56 +0800

    test

---
+++ bmark/generate_test.go
@@ -0,0 +1,40 @@
+// generate_test.go
+package main
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func generateWithCap(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func generate(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func BenchmarkGenerateWithCap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generateWithCap(1000000)
+	}
+}
+
+func BenchmarkGenerate(b *testing.B) {
+	time.Sleep(time.Second * 3) // 模拟耗时准备任务
+	b.ResetTimer()              // 重置定时器
+	for n := 0; n < b.N; n++ {
+		generate(1000000)
+	}
+}
---
+++ bmark/readme.md
@@ -0,0 +1,8 @@
+# benchmark
+
+    go test -bench='Generate' .
+
+    -benchtime=6s
+    -benchtime=30x
+    -count=50
+    -benchmem 参数可以度量内存分配的次数
